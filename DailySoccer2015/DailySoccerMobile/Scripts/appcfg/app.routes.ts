01dccab6885fde6a2e6d2829cd0ad6a162fa7b10
Teerachai Laothong <teerachai@perfenterprise.com>
2015-12-22 08:18:40 +0700

    Add app.routes.ts into appcfg.
    Signed-off-by: Teerachai Laothong <teerachai@perfenterprise.com>

---
+++ DailySoccer2015/DailySoccerMobile/Scripts/appcfg/app.routes.ts
@@ -0,0 +1,34 @@
+﻿module app {
+	'use strict';
+
+	angular
+		.module('app')
+		.config(configRoutes);
+
+	configRoutes.$inject = [
+		'$stateProvider',
+		'$urlRouterProvider'
+	];
+	function configRoutes($stateProvider: ng.ui.IStateProvider, $urlRouterProvider: ng.ui.IUrlRouterProvider) {
+		//$stateProvider.state('default', {
+		//	url: '/default',
+		//	templateUrl: 'templateUrl',
+		//	controller: 'ControllerFullName',
+		//	controllerAs: 'cx'
+		// }).state('stateName', {
+		//     url: '/url/to/navigate',
+		//     templateUrl: 'url/to/template/:sampleId',
+		//     controller: 'ControllerFullName',
+		//     controllerAs: 'cx',
+		//     resolve: {
+		//         'ctlrInputId': ['$stateParams', '$q', ($stateParams: ng.ui.IStateParamsService, $q: ng.IQService): ng.IPromise<number> => {
+		//             var defer = $q.defer<number>();
+		//             defer.resolve((<any>$stateParams).sampleId);
+		//             return defer.promise;
+		//         }]
+		//     }
+		//});
+
+		//$urlRouterProvider.otherwise('/home');
+	}
+}
