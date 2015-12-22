0e2ad2abf43bcc75e197a51d9f8de0f205033756
Teerachai Laothong <teerachai@perfenterprise.com>
2015-12-22 10:12:19 +0700

    Add a ApiApp (webapi project).
    Signed-off-by: Teerachai Laothong <teerachai@perfenterprise.com>

---
+++ DailySoccer2015/ApiApp/Global.asax.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Http;
+using System.Web.Routing;
+
+namespace ApiApp
+{
+    /// <summary>
+    /// The Http Application for WebApi
+    /// </summary>
+    public class WebApiApplication : System.Web.HttpApplication
+    {
+        /// <summary>
+        /// Invokes when the webapi application start
+        /// </summary>
+        protected void Application_Start()
+        {
+            GlobalConfiguration.Configure(WebApiConfig.Register);
+        }
+    }
+}
