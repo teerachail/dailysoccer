6935ac3ae9e355cdb470cc323c3fd3a77ad73dfd
Sakul Jaruthanaset <miolynet@perfenterprise.com>
2015-12-26 18:55:43 +0700

    Add MongoDB's helper class & database connection string complete.
    Signed-off-by: Sakul Jaruthanaset <miolynet@perfenterprise.com>

--- DailySoccer2015/ApiApp/MongoAccess/MongoUtil.cs
+++ DailySoccer2015/ApiApp/MongoAccess/MongoUtil.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using MongoDB.Driver;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -9,6 +10,18 @@ namespace ApiApp.MongoAccess
 {
     static class MongoUtil
     {
+        private static IMongoClient _client;
+        private static IMongoDatabase _database;
+
+        static MongoUtil()
+        {
+            var connectionString = WebConfigurationManager.AppSettings["primaryConnectionString"];
+            _client = new MongoClient(connectionString);
+
+            var dbName = WebConfigurationManager.AppSettings["databaseName"];
+            _database = _client.GetDatabase(dbName);
+        }
+
         public static string /*MongoCollection*/ GetCollection(string collectionName)
         {
             //var connectionString = WebConfigurationManager.ConnectionStrings["primaryConnectionString"].ConnectionString;
