29afffa322cbbf775f3bb16fb6ae566676f802df
Sakul Jaruthanaset <miolynet@perfenterprise.com>
2015-12-26 22:54:23 +0700

    Add all repositories complete.
    Signed-off-by: Sakul Jaruthanaset <miolynet@perfenterprise.com>

---
+++ DailySoccer2015/ApiApp/Repositories/MarkerRepository.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ApiApp.Repositories
+{
+    /// <summary>
+    /// ตัวติดต่อกับตัวติดตามแมช์การแข่งขัน
+    /// </summary>
+    public class MarkerRepository : IMarkerRepository
+    {
+    }
+}
