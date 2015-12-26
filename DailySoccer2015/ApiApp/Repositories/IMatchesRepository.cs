e01da59da04ea5b5d920b1de2d0fe6376c6c04f0
Sakul Jaruthanaset <miolynet@perfenterprise.com>
2015-12-26 20:57:12 +0700

    Move prediction's functions from MatchesRepository to PredictionRepository complete.
    Signed-off-by: Sakul Jaruthanaset <miolynet@perfenterprise.com>

--- DailySoccer2015/ApiApp/Repositories/IMatchesRepository.cs
+++ DailySoccer2015/ApiApp/Repositories/IMatchesRepository.cs
@@ -20,30 +20,6 @@ namespace ApiApp.Repositories
         /// <param name="date">วันที่ที่ต้องการดึงข้อมูล</param>
         IEnumerable<MatchInformation> GetMatchesByDate(DateTime date);
 
-        /// <summary>
-        /// ดึงการทายผลของผู้ใช้จากวันที่
-        /// </summary>
-        /// <param name="userId">รหัสบัญชีผู้ใช้ที่ต้องการดึง</param>
-        /// <param name="date">วันที่ที่ต้องการดึงข้อมูล</param>
-        IEnumerable<PredictionInformation> GetUserPredictionsByDate(string userId, DateTime date);
-
-        /// <summary>
-        /// กำหนดการทายผลของผู้ใช้
-        /// </summary>
-        /// <param name="userId">รหัสบัญชีผู้ใช้ที่ต้องการกำหนด</param>
-        /// <param name="matchId">รหัสแมช์การแข่งขันที่ผู้ใช้เลือก</param>
-        /// <param name="winnerTeamId">รหัสทีมที่ผู้ใช้เลือก</param>
-        /// <param name="predictionPoints">คะแนนที่ผู้ใช้จะได้ถ้าทายถูก</param>
-        /// <param name="currentTime">วันเวลาที่ทำการบันทึกข้อมูล</param>
-        void SetUserPrediction(string userId, string matchId, string winnerTeamId, int predictionPoints, DateTime currentTime);
-
-        /// <summary>
-        /// ยกเลิกการทายผลของผู้ใช้
-        /// </summary>
-        /// <param name="userId">รหัสบัญชีผู้ใช้ที่ต้องการกำหนด</param>
-        /// <param name="matchId">รหัสแมช์การแข่งขันที่ผู้ใช้ยกเลิก</param>
-        void CancelUserPrediction(string userId, string matchId);
-
         #endregion Methods
     }
 }
--- DailySoccer2015/ApiApp/Repositories/MatchesRepository.cs
+++ DailySoccer2015/ApiApp/Repositories/MatchesRepository.cs
@@ -23,43 +23,7 @@ namespace ApiApp.Repositories
             // TODO: GetMatchesByDate
             throw new NotImplementedException();
         }
-
-        /// <summary>
-        /// ดึงการทายผลของผู้ใช้จากวันที่
-        /// </summary>
-        /// <param name="userId">รหัสบัญชีผู้ใช้ที่ต้องการดึง</param>
-        /// <param name="date">วันที่ที่ต้องการดึงข้อมูล</param>
-        public IEnumerable<PredictionInformation> GetUserPredictionsByDate(string userId, DateTime date)
-        {
-            // TODO: GetUserPredictionsByDate
-            throw new NotImplementedException();
-        }
-
-        /// <summary>
-        /// กำหนดการทายผลของผู้ใช้
-        /// </summary>
-        /// <param name="userId">รหัสบัญชีผู้ใช้ที่ต้องการกำหนด</param>
-        /// <param name="matchId">รหัสแมช์การแข่งขันที่ผู้ใช้เลือก</param>
-        /// <param name="winnerTeamId">รหัสทีมที่ผู้ใช้เลือก</param>
-        /// <param name="predictionPoints">คะแนนที่ผู้ใช้จะได้ถ้าทายถูก</param>
-        /// <param name="currentTime">วันเวลาที่ทำการบันทึกข้อมูล</param>
-        public void SetUserPrediction(string userId, string matchId, string winnerTeamId, int predictionPoints, DateTime currentTime)
-        {
-            // TODO: SetUserPrediction
-            throw new NotImplementedException();
-        }
-
-        /// <summary>
-        /// ยกเลิกการทายผลของผู้ใช้
-        /// </summary>
-        /// <param name="userId">รหัสบัญชีผู้ใช้ที่ต้องการกำหนด</param>
-        /// <param name="matchId">รหัสแมช์การแข่งขันที่ผู้ใช้ยกเลิก</param>
-        public void CancelUserPrediction(string userId, string matchId)
-        {
-            // TODO: CancelUserPrediction
-            throw new NotImplementedException();
-        }
-
+        
         #endregion IMatchesRepository members
     }
 }
