90c37df4e3ab339741b5cc1767cdc9abd003a5b6
Sakul Jaruthanaset <miolynet@perfenterprise.com>
2015-12-28 00:24:39 +0700

    Implement GET api/leagues complete.
    Signed-off-by: Sakul Jaruthanaset <miolynet@perfenterprise.com>

---
+++ DailySoccer2015/ApiApp/Controllers/LeaguesController.cs
@@ -0,0 +1,39 @@
+﻿using ApiApp.Models;
+using ApiApp.Repositories;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace ApiApp.Controllers
+{
+    /// <summary>
+    /// Leagues API
+    /// </summary>
+    [RoutePrefix("api/leagues")]
+    public class LeaguesController : ApiController
+    {
+        private IMatchesRepository _repo;
+
+        /// <summary>
+        /// Initialize Leagues API
+        /// </summary>
+        /// <param name="repo">Matches repository</param>
+        public LeaguesController(IMatchesRepository repo)
+        {
+            _repo = repo;
+        }
+
+        // GET: api/Leagues
+        /// <summary>
+        /// Get all leagues
+        /// </summary>
+        [HttpGet]
+        public IEnumerable<League> Get()
+        {
+            return _repo.GetAllLeagues();
+        }
+    }
+}
