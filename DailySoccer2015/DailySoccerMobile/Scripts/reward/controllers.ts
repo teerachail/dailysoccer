1b23fcc15bb2933a3f9139e31f54b66c289f400c
Sakul Jaruthanaset <miolynet@perfenterprise.com>
2015-12-30 02:18:02 +0700

    Implementing buy coupon processing incomplete.
    Signed-off-by: Sakul Jaruthanaset <miolynet@perfenterprise.com>

--- DailySoccer2015/DailySoccerMobile/Scripts/reward/controllers.ts
+++ DailySoccer2015/DailySoccerMobile/Scripts/reward/controllers.ts
@@ -26,23 +26,68 @@
     }
 
     class BuyCouponProcessingController {
-        static $inject = ['$scope', '$ionicModal', '$state', 'app.reward.BuyCouponDataService'];
-        constructor(private $scope, private $ionicModal, private $state: angular.ui.IStateService, private buySvc: app.reward.BuyCouponDataService) {
+        static $inject = ['$scope', '$timeout', '$ionicModal', '$state', 'app.reward.BuyCouponDataService', 'app.shared.UserProfileService'];
+        constructor(private $scope, private $timeout: ng.ITimeoutService, private $ionicModal, private $state: angular.ui.IStateService, private buySvc: app.reward.BuyCouponDataService, private userprofileSvc: app.shared.UserProfileService) {
             this.$ionicModal.fromTemplateUrl('templates/Facebook.html',
                 {
                     scope: $scope,
                     animation: 'slide-in-up'
-                }).then(function (modal): void { $scope.FacebookPopup = modal; });
+                }).then(modal=> { this.$scope.FacebookPopup = modal; });
             this.$ionicModal.fromTemplateUrl('templates/Tie.html',
                 {
                     scope: $scope,
                     animation: 'slide-in-up'
-                }).then(function (modal): void { $scope.TiePopup = modal; });
+                }).then(modal=> { this.$scope.TiePopup = modal; });
             this.$ionicModal.fromTemplateUrl('templates/BuyCouponComplete.html',
                 {
                     scope: $scope,
                     animation: 'slide-in-up'
-                }).then(function (modal): void { $scope.CompletedPopup = modal; });
+                }).then(modal=> { this.$scope.CompletedPopup = modal; });
+
+            this.$scope.$on('modal.hidden', () => this.validateUserProfile());
+            this.validateUserProfile();
+        }
+
+        private validateUserProfile(): void {
+            if (!this.checkPreparingPopups()) return;
+
+            if (!this.checkFacebookAuthentication()) return;
+            else console.log('Facebook authentication verified');
+
+            if (!this.checkPhoneVerification()) return;
+            else console.log('Phone number verified');
+        }
+        private checkPreparingPopups(): boolean {
+            var isSetupCompleted = this.$scope.FacebookPopup != null
+                && this.$scope.TiePopup != null
+                && this.$scope.CompletedPopup != null;
+            if (!isSetupCompleted) {
+                console.log('Preparing...');
+                this.$timeout(() => this.validateUserProfile(), 333);
+            }
+            return isSetupCompleted;
+        }
+        private checkFacebookAuthentication(): boolean {
+            var userprofile = this.userprofileSvc.GetUserProfile();
+            if (!userprofile.IsVerifiedFacebook) {
+                if (this.buySvc.CheckFirstTimeForRequestFacebookLogin()) this.$scope.FacebookPopup.show();
+                else this.gobackToBuyCouponPage();
+            }
+            return userprofile.IsVerifiedFacebook;
+
+        }
+        private checkPhoneVerification(): boolean {
+            var userprofile = this.userprofileSvc.GetUserProfile();
+            var isPhoneNumberVerified = userprofile.VerifiedPhoneNumber != null;
+            if (!isPhoneNumberVerified) {
+                if (this.buySvc.CheckFirstTimeForRequestPhoneVerification()) this.$state.go('app.phone');
+                else this.gobackToBuyCouponPage();
+            }
+            return isPhoneNumberVerified;
+        }
+        private gobackToBuyCouponPage(): void {
+            this.buySvc.ResetAllRequests();
+            this.$state.go('app.buy');
         }
     }
 
--- DailySoccer2015/DailySoccerMobile/Scripts/reward/services.ts
+++ DailySoccer2015/DailySoccerMobile/Scripts/reward/services.ts
@@ -6,6 +6,8 @@
         public RemainingPoints: number;
         public CouponCost: number;
         public BuyingPower: number;
+        private isAlreadyRequireFacebookLogin: boolean;
+        private isAlreadyRequirePhoneVerification: boolean;
 
         public InitialData(couponCost: number, remainingPoints: number): void {
             this.CouponCost = couponCost;
@@ -13,8 +15,25 @@
             const NotAvailableCost = 0;
             var isCouponCostValid = couponCost > NotAvailableCost;
             this.BuyingPower = isCouponCostValid ? Math.floor(this.RemainingPoints / this.CouponCost) : NotAvailableCost;
+            this.ResetAllRequests();
         }
 
+        public CheckFirstTimeForRequestFacebookLogin(): boolean {
+            var beforeValueChanged = this.isAlreadyRequireFacebookLogin;
+            this.isAlreadyRequireFacebookLogin = false;
+            return beforeValueChanged;
+        }
+
+        public CheckFirstTimeForRequestPhoneVerification(): boolean {
+            var beforeValueChanged = this.isAlreadyRequirePhoneVerification;
+            this.isAlreadyRequirePhoneVerification = false;
+            return beforeValueChanged;
+        }
+
+        public ResetAllRequests(): void {
+            this.isAlreadyRequireFacebookLogin = true;
+            this.isAlreadyRequirePhoneVerification = true;
+        }
     }
 
 	angular
