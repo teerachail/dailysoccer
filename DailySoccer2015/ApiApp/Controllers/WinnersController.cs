1a58a804c89ed3a5c6bfe9aa9d049e129d607f1d
Sakul Jaruthanaset <miolynet@perfenterprise.com>
2016-01-15 11:43:36 +0700

    Create new web API for setting the winners incomplete.
    Signed-off-by: Sakul Jaruthanaset <miolynet@perfenterprise.com>

---
+++ DailySoccer2015/ApiApp/Controllers/WinnersController.cs
@@ -0,0 +1,47 @@
+﻿using ApiApp.Repositories;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace ApiApp.Controllers
+{
+    /// <summary>
+    /// Winners API
+    /// </summary>
+    public class WinnersController : ApiController
+    {
+        private IRewardRepository _rewardRepo;
+        private IAccountRepository _accountRepo;
+
+        /// <summary>
+        /// Initialize Winners API
+        /// </summary>
+        /// <param name="rewardRepo">Reward repository</param>
+        /// <param name="accountRepo">Account repository</param>
+        public WinnersController(IRewardRepository rewardRepo, IAccountRepository accountRepo)
+        {
+            _rewardRepo = rewardRepo;
+            _accountRepo = accountRepo;
+        }
+
+        // POST: api/Winners
+        /// <summary>
+        /// Randrom the reward winners
+        /// </summary>
+        public void Post()
+        {
+        }
+
+        // PUT: api/Winners
+        /// <summary>
+        /// Force end the current reward group
+        /// </summary>
+        public void Put()
+        {
+
+        }
+    }
+}
