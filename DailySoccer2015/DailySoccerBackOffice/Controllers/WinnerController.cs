caf171781729756d2b041103c9aaa74b286f1c7e
Sakul Jaruthanaset <miolynet@perfenterprise.com>
2016-01-15 15:47:27 +0700

    Implement back office for manage rewards' winners complete.
    Signed-off-by: Sakul Jaruthanaset <miolynet@perfenterprise.com>

---
+++ DailySoccer2015/DailySoccerBackOffice/Controllers/WinnerController.cs
@@ -0,0 +1,41 @@
+﻿using RestSharp;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace DailySoccerBackOffice.Controllers
+{
+    public class WinnerController : Controller
+    {
+        // GET: Winner
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public string EndCurrentRewardGroup()
+        {
+            callDailySoccerAPI(Method.PUT);
+            return "Current reward group was ended.";
+        }
+
+        [HttpPost]
+        public string RandomWinner()
+        {
+            callDailySoccerAPI(Method.POST);
+            return "Random the winners was completed.";
+        }
+
+        private void callDailySoccerAPI(Method method)
+        {
+            const string ClientBaseURL = "http://dailysoccer-joker.azurewebsites.net";
+            var client = new RestClient(ClientBaseURL);
+            var request = new RestRequest("api/winners", method);
+            var respond = client.Execute(request);
+            var content = respond.Content;
+        }
+    }
+}
